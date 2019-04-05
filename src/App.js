18e69f88d436560dd828caada1d9cae8b3643f1c
naim <thanvirdx919@gmail.com>
2019-04-05 17:21:33 -0400

    all files added

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import NavBar from "./components/navBar";
+import Counters from "./components/counters";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters: counters });
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDecrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
+    if (counters[index].value < 0) {
+      counters[index].value = 0;
+    }
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = [...this.state.counters];
+    counters.forEach(function(counter) {
+      counter.value = 0;
+    });
+
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onReset={this.handleReset}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/counter.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeColor()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-success btn-sm m-2"
+        >
+          +
+        </button>
+        <button
+          onClick={() => this.props.onDecrement(this.props.counter)}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          -
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeColor() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value: count } = this.props.counter;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
